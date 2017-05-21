ee82727aabd54d9c78bdb0c813e01d36551f1356
jxu7 <jxu7@bsu.edu>
2017-05-21 11:12:57 -0400

    added a 11 layers resnext simplenet-v2

--- planet_models/simplenet_v2.py
+++ planet_models/simplenet_v2.py
@@ -1,22 +1,22 @@
-import torch
 import torch.nn as nn
+import math
 
 
 class Block(nn.Module):
     def __init__(self, inpt_channel, output_channel):
         super(Block, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=inpt_channel, kernel_size=1, out_channels=output_channel/2,
-                               padding=0, stride=1)
-        self.bn1 = nn.BatchNorm2d(output_channel/2)
+        self.conv1 = nn.Conv2d(in_channels=inpt_channel, kernel_size=1, out_channels=output_channel//2, bias=False,
+                               stride=1)
+        self.bn1 = nn.BatchNorm2d(output_channel//2)
         self.elu1 = nn.ELU(inplace=True)
 
-        self.conv2 = nn.Conv2d(in_channels=output_channel/2, out_channels=output_channel/2,
-                               kernel_size=3, stride=2, padding=1)
-        self.bn2 = nn.BatchNorm2d(output_channel/2)
+        self.conv2 = nn.Conv2d(in_channels=output_channel//2, out_channels=output_channel//2, bias=False,
+                               kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(output_channel//2)
         self.elu2 = nn.ELU(inplace=True)
 
-        self.conv3 = nn.Conv2d(in_channels=output_channel/2, out_channels=output_channel, kernel_size=1, stride=1,
-                               padding=0)
+        self.conv3 = nn.Conv2d(in_channels=output_channel//2, out_channels=output_channel, kernel_size=1, bias=False,
+                               stride=1)
         self.bn3 = nn.BatchNorm2d(output_channel)
         self.elu3 = nn.ELU(inplace=True)
 
@@ -31,8 +31,82 @@ class Block(nn.Module):
 
 
 class SimpleNetV2(nn.Module):
-    def __init__(self, inpt_size=64, input_channel=3):
+    def __init__(self, inpt_size=72, input_channel=3):
         super(SimpleNetV2, self).__init__()
+        # (3, 72, 72)
+        self.stage_1 = Block(inpt_channel=input_channel, output_channel=128)
+        self.maxpool_1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        # (128, 36, 36)
+        self.stage_2 = Block(inpt_channel=128, output_channel=256)
+        self.maxpool_2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        # (512, 18, 18)
+        self.stage_3 = Block(inpt_channel=256, output_channel=512)
+        self.maxpool_3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(512, 2048, kernel_size=1, stride=1, bias=False),
+            nn.BatchNorm2d(2048),
+        )
+        self.avg_pool3 = nn.AvgPool2d(5)
+        # (1024, 9, 9)
+        self.stage_4 = Block(inpt_channel=512, output_channel=1024)
+        self.maxpool_4 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(1024, 2048, kernel_size=1, stride=1, bias=False),
+            nn.BatchNorm2d(2048),
+        )
+        self.avg_pool4 = nn.AvgPool2d(3)
+        # (1024, 4, 4)
+        self.stage_5 = Block(inpt_channel=1024, output_channel=2048)
+        self.maxpool_5 = nn.MaxPool2d(kernel_size=2, stride=2)
+        # (2048, 2, 2)
+        self.avg_pool5 = nn.AvgPool2d(2)
+
+        self.elu5 = nn.ELU(inplace=True)
+
+        self.classifier = nn.Sequential(
+            nn.Linear(2048, 512, bias=False),
+            nn.BatchNorm1d(512),
+            nn.ELU(inplace=True),
+            nn.Dropout(p=0.3, inplace=True),
+            nn.Linear(512, 17)
+        )
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
 
     def forward(self, x):
-        pass
+        # stage 1
+        x = self.stage_1(x)
+        x = self.maxpool_1(x)
+
+        # stage 2
+        x = self.stage_2(x)
+        x = self.maxpool_2(x)
+
+        # stage 3
+        x = self.stage_3(x)
+        x = self.maxpool_3(x)
+        residual_3 = self.conv3(x)
+        residual_3 = self.avg_pool3(residual_3)
+
+        # stage 4
+        x = self.stage_4(x)
+        x = self.maxpool_4(x)
+        residual_4 = self.conv4(x)
+        residual_4 = self.avg_pool4(residual_4)
+        # stage 5
+        x = self.stage_5(x)
+        x = self.maxpool_5(x)
+
+        x = self.avg_pool5(x)
+
+        x = x + residual_3 + residual_4
+        x = self.elu5(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
---
+++ trainers/train_simplenet_v2.py
@@ -0,0 +1,93 @@
+from torch.nn import *
+from util import *
+from torch import optim
+from planet_models.simplenet_v2 import SimpleNetV2
+from datasets import *
+import torch
+
+
+name = 'simplenet_v2'
+is_cuda_availible = torch.cuda.is_available()
+
+
+def train_simplenet_v2_forest(epoch=50):
+    criterion = MultiLabelSoftMarginLoss()
+    net = SimpleNetV2()
+    logger = Logger('../log/', name)
+    optimizer = optim.Adam(lr=1e-4, params=net.parameters(), weight_decay=5e-4)
+    net.cuda()
+    resnet = torch.nn.DataParallel(net, device_ids=[0, 1])
+    train_data_set = train_jpg_loader(256, transform=Compose(
+        [
+            RandomHorizontalFlip(),
+            Scale(100),
+            RandomCrop(72),
+            ToTensor(),
+            Normalize(mean, std)
+        ]
+    ))
+    validation_data_set = validation_jpg_loader(64, transform=Compose(
+        [
+            Scale(72),
+            ToTensor(),
+            Normalize(mean, std)
+         ]
+    ))
+    best_loss = np.inf
+    patience = 0
+    for i in range(epoch):
+        # training
+        for batch_index, (target_x, target_y) in enumerate(train_data_set):
+            if is_cuda_availible:
+                target_x, target_y = target_x.cuda(), target_y.cuda()
+            resnet.train()
+            target_x, target_y = Variable(target_x), Variable(target_y)
+            optimizer.zero_grad()
+            output = resnet(target_x)
+            loss = criterion(output, target_y)
+            loss.backward()
+            optimizer.step()
+            if batch_index % 50 == 0:
+                print('Training loss is {}'.format(loss.data[0]))
+        print('Finished epoch {}'.format(i))
+
+        # evaluating
+        val_loss = 0.0
+        f2_scores = 0.0
+        resnet.eval()
+        for batch_index, (val_x, val_y) in enumerate(validation_data_set):
+            if is_cuda_availible:
+                val_y = val_y.cuda()
+            val_y = Variable(val_y, volatile=True)
+            val_output = evaluate(resnet, val_x)
+            val_loss += criterion(val_output, val_y)
+            binary_y = threshold_labels(val_output.data.cpu().numpy())
+            f2 = f2_score(val_y.data.cpu().numpy(), binary_y)
+            f2_scores += f2
+        val_loss = val_loss.data[0]/batch_index
+        if best_loss > val_loss:
+            print('Saving model...')
+            best_loss = val_loss
+            torch.save(resnet.state_dict(), '../models/{}.pth'.format(resnet_name))
+            patience = 0
+        else:
+            patience += 1
+            print('Patience: {}'.format(patience))
+            print('Best loss {}, previous loss {}'.format(best_loss, val_loss))
+
+        if patience >= 20:
+            print('Early stopping!')
+            break
+
+        print('Evaluation loss is {}, Training loss is {}'.format(val_loss, loss.data[0]))
+        print('F2 Score is %s' % (f2_scores/batch_index))
+        logger.add_record('train_loss', loss.data[0])
+        logger.add_record('evaluation_loss', val_loss)
+        logger.add_record('f2_score', f2_scores/batch_index)
+    logger.save()
+    logger.save_plot()
+
+
+if __name__ == '__main__':
+    train_simplenet_v2_forest(epoch=85)
+
