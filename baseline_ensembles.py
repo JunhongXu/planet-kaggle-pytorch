4bd4a182f91c0c1b4facbef95e2c55b9398ec9ee
jxu7 <jxu7@bsu.edu>
2017-06-22 19:42:55 -0400

    densenet161+densenet169+resnet152+densenet121

--- baseline_ensembles.py
+++ baseline_ensembles.py
@@ -51,15 +51,28 @@ def verticalFlip(imgs):
 
 mean = [0.31151703, 0.34061992, 0.29885209]
 std = [0.16730586, 0.14391145, 0.13747531]
-threshold = [0.23166666666666666, 0.19599999999999998, 0.18533333333333335,
-             0.08033333333333334, 0.20199999999999999, 0.16866666666666666,
-             0.20533333333333334, 0.27366666666666667, 0.2193333333333333,
-             0.21299999999999999, 0.15666666666666665, 0.096666666666666679,
-             0.21933333333333335, 0.058666666666666673, 0.19033333333333333,
-             0.25866666666666666, 0.057999999999999996]
+# threshold = [0.23166666666666666, 0.19599999999999998, 0.18533333333333335,
+#              0.08033333333333334, 0.20199999999999999, 0.16866666666666666,
+#              0.20533333333333334, 0.27366666666666667, 0.2193333333333333,
+#              0.21299999999999999, 0.15666666666666665, 0.096666666666666679,
+ #            0.21933333333333335, 0.058666666666666673, 0.19033333333333333,#           0.25866666666666666, 0.057999999999999996]
+
+# threshold = [ 0.18533333,  0.18866667, 0.13533333, 0.03633333, 0.221, 0.17666667,
+  # 0.231, 0.23933333, 0.21966667, 0.169, 0.23333333, 0.21833333,
+ #  0. 24033333, 0.112, 0.40233333, 0.31833333, 0.237]
+
+# threshold = [ 0.17733333, 0.213, 0.15766667, 0.049, 0.28733333, 0.18066667,
+ #  0.19666667, 0.212, 0.21566667, 0.17233333, 0.16466667, 0.274,
+ #  0.27833333, 0.10266667, 0.293, 0.241, 0.08366667]
+threshold = [ 0.142, 0.17, 0.122, 0.054, 0.188, 0.156, 0.228, 0.234, 0.142, 0.226,
+  0.188, 0.192, 0.192, 0.084, 0.242, 0.4, 0.126]
 
 transforms = [default, rotate90, rotate180, rotate270, verticalFlip, horizontalFlip]
 models = [# resnet18_planet, resnet34_planet, resnet50_planet, densenet121, densenet161, densenet169
+           # resnet152_planet
+    densenet121,
+    densenet169,
+            densenet161,
             resnet152_planet
         ]
 
@@ -96,7 +109,7 @@ def probs(dataloader):
 
 def find_best_threshold(labels, probabilities):
     threshold = np.zeros(17)
-
+    acc = 0
     # iterate over transformations
     for t_idx in range(len(transforms)):
         # iterate over class labels
@@ -118,6 +131,8 @@ def find_best_threshold(labels, probabilities):
             print('Transform index {}, score {}, threshold {}, label {}'.format(t_idx, best_score, best_thresh, i))
         print('Transform index {}, threshold {}, score {}'.format(t_idx, t, best_score))
         threshold = threshold + t
+        acc += best_score
+    print('AVG ACC,', acc/len(transforms))
     threshold = threshold / len(transforms)
     return threshold
 
@@ -150,16 +165,16 @@ def get_test_dataloader():
         label_csv=None
     )
 
-    test_dataloader = DataLoader(test_dataset, batch_size=512)
+    test_dataloader = DataLoader(test_dataset, batch_size=256)
     return test_dataloader
 
 
 def do_thresholding(names, labels):
     preds = np.empty((len(transforms), len(models), 3000, 17))
+    print('filenames', names)
     for t_idx in range(len(transforms)):
         for m_idx in range(len(models)):
             preds[t_idx, m_idx] = np.loadtxt(names[t_idx + m_idx])
-    print(names)
     t = find_best_threshold(labels=labels, probabilities=preds)
     return t
 
@@ -168,10 +183,8 @@ def get_files(excludes=['resnet18']):
     file_names = glob.glob('probs/*.txt')
     names = []
     for filename in file_names:
-        for exclude in excludes:
-            if filename not in exclude:
-                # file_names = [name for name in file_names if exclude not in name]
-                names.append(filename)
+        if not any([exclude in filename for exclude in excludes]):
+            names.append(filename)
     return names
 
 
@@ -186,20 +199,20 @@ def predict_test(t):
         preds = preds + pred
 
     preds = preds/len(models)
-    pred_csv(predictions=preds, threshold=t, name='ensembles')
+    pred_csv(predictions=preds, threshold=t, name='resnet152_densenet161_densent169_densent121-ensembels')
 
 
 if __name__ == '__main__':
-    valid_dataloader = get_validation_loader()
+    # valid_dataloader = get_validation_loader()
     test_dataloader = get_test_dataloader()
 
     # save results to files
-    probabilities = probs(valid_dataloader)
+    # probabilities = probs(valid_dataloader)
 
     # get threshold
-    file_names = get_files(['resnet18', 'resnet50', 'resnet34', 'densenet121', 'densenet169'])
-    t = do_thresholding(file_names, valid_dataloader.dataset.labels)
-    print(t)
+    # file_names = get_files(['resnet18', 'resnet50', 'resnet34'])
+    # t = do_thresholding(file_names, valid_dataloader.dataset.labels)
+   #  print(t)
 
     # testing
-    # predict_test(threshold)
+    predict_test(threshold)
