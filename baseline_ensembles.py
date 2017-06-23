c0c1200f6f1eceb592fb0eefe4db5bfd35354988
Junhong Xu <jxu7@bsu.edu>
2017-06-23 08:35:20 +0830

    resnet152-densenet161-densenet169-densenet121

--- baseline_ensembles.py
+++ baseline_ensembles.py
@@ -55,23 +55,28 @@ std = [0.16730586, 0.14391145, 0.13747531]
 #              0.08033333333333334, 0.20199999999999999, 0.16866666666666666,
 #              0.20533333333333334, 0.27366666666666667, 0.2193333333333333,
 #              0.21299999999999999, 0.15666666666666665, 0.096666666666666679,
- #            0.21933333333333335, 0.058666666666666673, 0.19033333333333333,#           0.25866666666666666, 0.057999999999999996]
+#            0.21933333333333335, 0.058666666666666673, 0.19033333333333333, 0.25866666666666666, 0.057999999999999996]
 
 # threshold = [ 0.18533333,  0.18866667, 0.13533333, 0.03633333, 0.221, 0.17666667,
-  # 0.231, 0.23933333, 0.21966667, 0.169, 0.23333333, 0.21833333,
- #  0. 24033333, 0.112, 0.40233333, 0.31833333, 0.237]
+#               0.231, 0.23933333, 0.21966667, 0.169, 0.23333333, 0.21833333,
+#               0.24033333, 0.112, 0.40233333, 0.31833333, 0.237]
 
 # threshold = [ 0.17733333, 0.213, 0.15766667, 0.049, 0.28733333, 0.18066667,
- #  0.19666667, 0.212, 0.21566667, 0.17233333, 0.16466667, 0.274,
- #  0.27833333, 0.10266667, 0.293, 0.241, 0.08366667]
-threshold = [ 0.142, 0.17, 0.122, 0.054, 0.188, 0.156, 0.228, 0.234, 0.142, 0.226,
-  0.188, 0.192, 0.192, 0.084, 0.242, 0.4, 0.126]
+#               0.19666667, 0.212, 0.21566667, 0.17233333, 0.16466667, 0.274,
+#               0.27833333, 0.10266667, 0.293, 0.241, 0.08366667]
+
+# threshold = [ 0.142, 0.17, 0.122, 0.054, 0.188, 0.156, 0.228, 0.234, 0.142, 0.226,
+#               0.188, 0.192, 0.192, 0.084, 0.242, 0.4, 0.126]
+
+threshold = [0.136, 0.236, 0.144, 0.044, 0.226, 0.152, 0.214, 0.218, 0.162, 0.204,
+             0.194, 0.19, 0.234, 0.066, 0.236, 0.188, 0.106]
 
 transforms = [default, rotate90, rotate180, rotate270, verticalFlip, horizontalFlip]
+
 models = [# resnet18_planet, resnet34_planet, resnet50_planet, densenet121, densenet161, densenet169
            # resnet152_planet
-    densenet121,
-    densenet169,
+            densenet121,
+            densenet169,
             densenet161,
             resnet152_planet
         ]
@@ -190,16 +195,23 @@ def get_files(excludes=['resnet18']):
 
 def predict_test(t):
     preds = np.zeros((61191, 17))
+    # imgs = test_dataloader.dataset.images.copy()
+    # iterate over models
     for index, model in enumerate(models):
         name = str(model).split()[1]
         net = nn.DataParallel(model().cuda())
-        net.eval()
         net.load_state_dict(torch.load('models/{}.pth'.format(name)))
+        net.eval()
+        # iterate over transformations
+        # for transformation in transforms:
+        #     imgs = transformation(imgs)
+        #     test_dataloader.dataset.images = imgs
         pred = predict(dataloader=test_dataloader, net=net)
         preds = preds + pred
 
-    preds = preds/len(models)
-    pred_csv(predictions=preds, threshold=t, name='resnet152_densenet161_densent169_densent121-ensembels')
+    # preds = preds/(len(models) * len(transforms))
+    preds = preds / len(models)
+    pred_csv(predictions=preds, threshold=t, name='resnet152_densenet161_densent169-ensembels')
 
 
 if __name__ == '__main__':
@@ -212,7 +224,7 @@ if __name__ == '__main__':
     # get threshold
     # file_names = get_files(['resnet18', 'resnet50', 'resnet34'])
     # t = do_thresholding(file_names, valid_dataloader.dataset.labels)
-   #  print(t)
+    # print(t)
 
     # testing
     predict_test(threshold)
