7b35b299af888c9ed2dd8642d6fbb9d6d3e23225
Junhong Xu <jxu7@bsu.edu>
2017-07-04 23:45:55 +0830

    fpn

--- planet_models/fpn.py
+++ planet_models/fpn.py
@@ -1,32 +1,85 @@
 import torch.nn as nn
-from torch.nn import functional as F
 import math
-from collections import OrderedDict
+from torch.nn import functional as F
 import torch
-from torch.autograd import Variable
+from torchvision.models.resnet import model_zoo, model_urls
 
 
-def _make_conv_bn_elu(in_channels, out_channels, kernel_size=3, stride=1, padding=1):
-    return OrderedDict([
-        ('conv2d', nn.Conv2d(in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride,
-                             padding=padding, bias=False)),
-        ('batch_norm', nn.BatchNorm2d(out_channels)),
-        ('elu', nn.ELU(inplace=True))
-    ])
+def fpn_50(pretrained=True):
+    model = ResNet(Bottleneck, [3, 4, 6, 3], num_classes=17)
+    if pretrained:
+        # load model dictionary
+        model_dict = model.state_dict()
+        # load pretrained model
+        pretrained_dict = model_zoo.load_url(model_urls['resnet50'])
+        # update model dictionary using pretrained model without classifier layer
+        model_dict.update({key: pretrained_dict[key] for key in pretrained_dict.keys() if 'fc' not in key})
+        model.load_state_dict(model_dict)
+    return model
 
 
-def _make_linear_bn_elu(in_units, output_units):
-    return OrderedDict(
-            [
-                ('linear', nn.Linear(in_units, output_units, bias=False)),
-                ('batch_norm', nn.BatchNorm1d(output_units)),
-                ('elu', nn.ELU()),
-            ]
-        )
+def fpn_152(pretrained=True):
+    model = ResNet(Bottleneck, [3, 8, 36, 3], num_classes=17)
+    if pretrained:
+        state_dict = model.state_dict()
+        pretrained_dict = model_zoo.load_url(model_urls['resnet152'])
+        pretrained_dict = {key: pretrained_dict[key] for key in pretrained_dict.keys() if 'fc' not in key}
+        state_dict.update(pretrained_dict)
+        model.load_state_dict(state_dict)
+    return model
+
+
+def fpn_34(pretrained=True):
+    model = ResNet(BasicBlock, [3, 4, 6, 3], num_classes=17)
+    if pretrained:
+        state_dict = model.state_dict()
+        pretrained_dict = model_zoo.load_url(model_urls['resnet34'])
+        pretrained_dict = {key: pretrained_dict[key] for key in pretrained_dict.keys() if 'fc' not in key}
+        state_dict.update(pretrained_dict)
+        model.load_state_dict(state_dict)
+    return model
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    "3x3 convolution with padding"
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
 
 
 class Bottleneck(nn.Module):
-    expansion = 2
+    expansion = 4
 
     def __init__(self, inplanes, planes, stride=1, downsample=None):
         super(Bottleneck, self).__init__()
@@ -35,8 +88,8 @@ class Bottleneck(nn.Module):
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
                                padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, planes * 2, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(planes * 2)
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
@@ -64,50 +117,36 @@ class Bottleneck(nn.Module):
         return out
 
 
-class FPNet(nn.Module):
+class ResNet(nn.Module):
 
-    def __init__(self, block, layers, input_channels=3, num_classes=17):
-        self.inplanes = 32
-        super(FPNet, self).__init__()
-        self.conv1 = nn.Sequential(
-            _make_conv_bn_elu(in_channels=input_channels, out_channels=32, kernel_size=3, stride=2, padding=0)
-        )                                                                            # 35*35*3
-        self.maxpool = nn.MaxPool2d(kernel_size=4, stride=1)                         # 32*32*36
+    def __init__(self, block, layers, num_classes=1000):
+        self.inplanes = 64
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)     # 64*64*64
+        # bottom-up
+        self.layer1 = self._make_layer(block, 64, layers[0])                # 256*64*64
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)     # 512*32*32
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)     # 1024*16*16
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)     # 2048*8*8
+        self.avgpool = nn.AvgPool2d(7)                                      # 2048*1*1
 
-        # downsampling
-        self.layer1 = self._make_layer(block, 64, layers[0], stride=2)               # 18*18*128
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)              # 8*8*256
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)              # 4*4*512
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)              # 2*2*1024
-        # downsampling prediction
-        self.d_pool = nn.AdaptiveAvgPool2d(1)
-        self.cls_d = nn.Sequential(_make_linear_bn_elu(in_units=1024, output_units=512))
+        # top-down
+        self.td_1 = self._make_top_down_layer(2048, 1024)                   # 1024*8*8
+        self.td_2 = self._make_top_down_layer(1024, 512)                    # 512*16*16
+        self.td_3 = self._make_top_down_layer(512, 256)                     # 256*32*32
 
-        # upsampling
-        self.layer3_up = nn.Sequential(_make_conv_bn_elu(self.inplanes//2, self.inplanes,
-                                                         kernel_size=1, stride=1, padding=0))    # 4*4*1024
-        self.layer2_up = nn.Sequential(_make_conv_bn_elu(self.inplanes//4, self.inplanes//2,
-                                                         kernel_size=1, stride=1, padding=0))  # 8*8*512
-        self.layer1_up = nn.Sequential(_make_conv_bn_elu(self.inplanes//8, self.inplanes//4,
-                                                         kernel_size=1, stride=1, padding=0) ) # 16*16*256
+        # extra conv layers
+        self.p1_conv = self._make_conv_bn(1024, 256, 3, padding=1, stride=1)
+        self.p2_conv = self._make_conv_bn(1024, 256, 3, padding=1, stride=1)
+        self.p3_conv = self._make_conv_bn(512, 256, 3, padding=1, stride=1)
+        self.p4_conv = self._make_conv_bn(256, 256, 3, padding=1, stride=1)
 
-        # final feature generation
-        self.f1 = nn.Sequential(_make_conv_bn_elu(256, 512, kernel_size=3, stride=2))  # 8*8*512
-        self.f2 = nn.Sequential(_make_conv_bn_elu(512, 512, kernel_size=3, stride=2))  # 4*4*512
-        self.f3 = nn.Sequential(_make_conv_bn_elu(1024, 512))                          # 4*4*512
-
-        # reduce dimensionality before classifier 1*1*256
-        self.pool1 = nn.AdaptiveAvgPool2d(1)
-        self.pool2 = nn.AdaptiveAvgPool2d(1)
-        self.pool3 = nn.AdaptiveAvgPool2d(1)
-
-        # clasifier
-        self.cls_1 = nn.Sequential(_make_linear_bn_elu(512, 512))
-        self.cls_2 = nn.Sequential(_make_linear_bn_elu(512, 512))
-        self.cls_3 = nn.Sequential(_make_linear_bn_elu(512, 512))
-
-        # logit
-        self.fc = nn.Linear(512*4, num_classes)
+        # classification layer
+        self.fc = nn.Linear(256 * 4, out_features=num_classes, bias=True)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -116,6 +155,23 @@ class FPNet(nn.Module):
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
+
+    def _make_top_down_layer(self, inpt_size, out_size):
+        # lateral connection
+        x = self._make_conv_bn(inpt_size=inpt_size, out_size=out_size, k_size=1,
+                                     padding=0, stride=1, use_relu=True)
+
+        return x
+
+    def _make_conv_bn(self, inpt_size, out_size, k_size, padding=1, stride=1, use_relu=False):
+        x = nn.Sequential(
+            nn.Conv2d(in_channels=inpt_size, bias=False, out_channels=out_size,
+                      kernel_size=k_size, stride=stride, padding=padding),
+            nn.BatchNorm2d(out_size)
+        )
+        if use_relu:
+            x.add_module('relu', nn.ReLU())
+        return x
 
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
@@ -136,65 +192,56 @@ class FPNet(nn.Module):
 
     def forward(self, x):
         x = self.conv1(x)
-        x = self.maxpool(x)     # 32*32*36 now using kernel 10*10, maybe smaller kernel?
-        # downsampling
-        d1 = self.layer1(x)     # 16*16*128
-        d2 = self.layer2(d1)     # 8*8*256
-        d3 = self.layer3(d2)     # 4*4*512
-        d4 = self.layer4(d3)     # 2*2*1024
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        # print(x.size())
 
-        # upsampling, will using transpose conv2d better?
-        up1 = F.upsample_nearest(d4, scale_factor=2)    # 4*4*1024
-        up2 = F.upsample_nearest(d3, scale_factor=2)    # 8*8*512
-        up3 = F.upsample_nearest(d2, scale_factor=2)    # 16*16*256
+        layer_1 = self.layer1(x)
+        # print(layer_1.size())
 
-        # lateral connection
-        l1 = self.layer1_up(d1)     # 16*16*256
-        l2 = self.layer2_up(d2)     # 8*8*512
-        l3 = self.layer3_up(d3)     # 4*4*1024
+        layer_2 = self.layer2(layer_1)
+        # print(layer_2.size())
 
-        # merge
-        m1 = l1 + up3   # 16*16*256
-        m2 = l2 + up2   # 8*8*512
-        m3 = l3 + up1   # 4*4*1024
+        layer_3 = self.layer3(layer_2)
+        # print(layer_3.size())
 
-        # extra convs
-        f1 = self.f1(m1)    # 8*8*256
-        f2 = self.f2(m2)    # 4*4*256
-        f3 = self.f3(m3)    # 2*2*256
+        layer_4 = self.layer4(layer_3)
+        # print(layer_4.size())
 
-        # max pool
-        f1 = self.pool1(f1)  # 512
-        f1 = f1.view(f1.size(0), -1)
-        f2 = self.pool2(f2)  # 512
-        f2 = f2.view(f2.size(0), -1)
-        f3 = self.pool3(f3)  # 512
-        f3 = f3.view(f3.size(0), -1)
+        p_1 = self.td_1(layer_4)
+        # print(p_1.size())
+        upsample_p1 = F.upsample_nearest(p_1, scale_factor=2)
 
-        # downsampling classifier
-        d_out = self.d_pool(d4)
-        d_out = d_out.view(d_out.size(0), -1)
-        d_out = self.cls_d(d_out)   # 512
+        p_2 = upsample_p1 + layer_3
+        # print(p_2.size())
+        upsample_p2 = F.upsample_nearest(p_2, scale_factor=2)
 
-        # classifier
-        cls1 = self.cls_1(f1)   # 512
-        cls2 = self.cls_2(f2)   # 512
-        cls3 = self.cls_3(f3)   # 512
+        p_3 = self.td_2(upsample_p2) + layer_2
+        # print(p_3.size())
+        upsample_p3 = F.upsample_nearest(p_3, scale_factor=2)
 
-        cls = torch.cat((cls1, cls2, cls3, d_out), dim=1)
+        p_4 = self.td_3(upsample_p3) + layer_1
+        # print(p_4.size())
 
-        logit = self.fc(cls)
+        p_1 = self.p1_conv(p_1)
+        p_2 = self.p2_conv(p_2)
+        p_3 = self.p3_conv(p_3)
+        p_4 = self.p4_conv(p_4)
 
-        prob = F.sigmoid(logit)
-        return logit, prob
+        # pooling
+        cls_1 = F.avg_pool2d(p_1, kernel_size=8)
+        cls_2 = F.avg_pool2d(p_2, kernel_size=16)
+        cls_3 = F.avg_pool2d(p_3, kernel_size=32)
+        cls_4 = F.avg_pool2d(p_4, kernel_size=64)
+        # print(cls_1.size(), cls_2.size(), cls_3.size(), cls_4.size())
+        # concatenate
+        cls = torch.cat([cls_1, cls_2, cls_3, cls_4], 1)
+        # print(cls.size())
+        cls = cls.view(cls.size(0), -1)
+        x = self.fc(cls)
+        return x
 
 
 if __name__ == '__main__':
-    import time
-    x = torch.randn((1, 3, 72, 72))
-    x = Variable(x).cuda()
-    net = FPNet(Bottleneck, [2, 2, 2, 2])
-    net.cuda()
-    t = time.time()
-    net(x)
-    print(time.time() - t)
+    fpn_34()
---
+++ trainers/fpn_trainer.py
@@ -0,0 +1,236 @@
+import torch.nn as nn
+from torch.nn import functional as F
+from torch import optim
+from torchvision.transforms import Lambda, Compose, Normalize
+from planet_models.fpn import fpn_34, fpn_50, fpn_152
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from datasets import mean, std
+from data.kgdataset import KgForestDataset, randomShiftScaleRotate, randomFlip, randomTranspose, toTensor
+from util import Logger, evaluate, multi_criterion, multi_f_measure, get_learning_rate
+import numpy as np
+import torch
+import time
+
+"""
+A FPN trainer trains the models as followed:
+FPN-34, FPN-50, FPN-152
+
+-------parameters---------
+    epochs: 60
+
+    batch size: 128, 128, 50
+
+    use SGD+0.9momentum w/o nestrov
+
+    weight decay: 5e-4
+
+    learning rate for pre-trained layers:
+                    00-10 epoch: 0.01
+                    10-25 epoch: 0.005
+                    25-40 epoch: 0.001
+                    40-60 epoch: 0.0001
+    learning rate for other layers:
+                    00-10 epoch: 0.1
+                    10-25 epoch: 0.05
+                    25-40 epoch: 0.01
+                    40-60 epoch: 0.001
+
+    train set: 40479
+"""
+
+
+models = [
+            fpn_34(),
+            # fpn_50(),
+           #  fpn_152()
+          ]
+batch_size = [
+                128,
+                128,
+                50
+            ]
+
+
+def get_dataloader(batch_size):
+    train_data = KgForestDataset(
+        split='train-37479',
+        transform=Compose(
+            [
+                Lambda(lambda x: randomShiftScaleRotate(x, u=0.75, shift_limit=6, scale_limit=6, rotate_limit=45)),
+                Lambda(lambda x: randomFlip(x)),
+                Lambda(lambda x: randomTranspose(x)),
+                Lambda(lambda x: toTensor(x)),
+                Normalize(mean=mean, std=std)
+            ]
+        ),
+        height=256,
+        width=256
+    )
+    train_data_loader = DataLoader(batch_size=batch_size, dataset=train_data, shuffle=True)
+
+    validation = KgForestDataset(
+        split='validation-3000',
+        transform=Compose(
+            [
+                # Lambda(lambda x: randomShiftScaleRotate(x, u=0.75, shift_limit=6, scale_limit=6, rotate_limit=45)),
+                Lambda(lambda x: randomFlip(x)),
+                Lambda(lambda x: randomTranspose(x)),
+                Lambda(lambda x: toTensor(x)),
+                Normalize(mean=mean, std=std)
+            ]
+        ),
+        height=256,
+        width=256
+    )
+
+    valid_dataloader = DataLoader(dataset=validation, shuffle=False, batch_size=batch_size)
+    return train_data_loader, valid_dataloader
+
+
+def get_optimizer(net, lr):
+    parameters = [
+                {'params': net.layer1.parameters(), 'lr': lr},
+                {'params': net.layer2.parameters(), 'lr': lr},
+                {'params': net.layer3.parameters(), 'lr': lr},
+                {'params': net.layer4.parameters(), 'lr': lr}
+            ]
+    optimizer = optim.SGD(params=parameters, lr=lr*10, weight_decay=5e-4, momentum=.9)
+    return optimizer
+
+
+def lr_schedule(epoch, optimizer, net):
+    if 0 <= epoch < 10:
+        lr = 0.01
+    elif 10 <= epoch < 25:
+        lr = 0.005
+    elif 25 <= epoch < 40:
+        lr = 0.001
+    else:
+        lr = 0.0001
+
+    param_groups = [
+        {'params': net.layer1.parameters(), 'lr': lr},
+        {'params': net.layer2.parameters(), 'lr': lr},
+        {'params': net.layer3.parameters(), 'lr': lr},
+        {'params': net.layer4.parameters(), 'lr': lr},
+        {'params': net.td_1.parameters(), 'lr': lr*10},
+        {'params': net.td_2.parameters(), 'lr': lr*10},
+        {'params': net.td_3.parameters(), 'lr': lr*10},
+        {'params': net.p1_conv.parameters(), 'lr': lr*10},
+        {'params': net.p2_conv.parameters(), 'lr': lr * 10},
+        {'params': net.p3_conv.parameters(), 'lr': lr * 10},
+        {'params': net.p4_conv.parameters(), 'lr': lr * 10},
+        {'weight_decay': 5e-4},
+        {'momentum': .9}
+    ]
+
+    # for para_group in optimizer.param_groups:
+    #     para_group['lr'] = lr
+    optimizer.param_groups = param_groups
+
+
+def load_net(net, name):
+    state_dict = torch.load('../models/{}.pth'.format(name))
+    net.load_state_dict(state_dict)
+
+
+def train_baselines():
+
+    train_data, val_data = get_dataloader(96)
+
+    for model, batch in zip(models, batch_size):
+        name = str(model).split()[1]
+        print('*****Start Training {} with batch size {}******'.format(name, batch))
+        print(' epoch   iter   rate  |  smooth_loss   |  train_loss  (acc)  |  valid_loss  (acc)  | total_train_loss\n')
+        logger = Logger('../log/full_data_{}'.format(name), name)
+
+        net = model(pretrained=True)
+        net = nn.DataParallel(net.cuda())
+        # load_net(net, name)
+        # optimizer = get_optimizer(net, lr=.001, pretrained=True, resnet=True if 'resnet' in name else False)
+        # optimizer = optim.SGD(lr=.005, momentum=0.9, params=net.parameters(), weight_decay=5e-4)
+        optimizer = get_optimizer(net, lr=0.01)
+        train_data.batch_size = batch
+        val_data.batch_size = batch
+
+        num_epoches = 40
+        print_every_iter = 20
+        epoch_test = 1
+
+        smooth_loss = 0.0
+        train_loss = np.nan
+        train_acc = np.nan
+        best_test_loss = np.inf
+        t = time.time()
+
+        for epoch in range(num_epoches):  # loop over the dataset multiple times
+
+            # train loss averaged every epoch
+            total_epoch_loss = 0.0
+
+            lr_schedule(epoch, optimizer, net)
+
+            rate = get_learning_rate(optimizer)[0]  # check
+
+            sum_smooth_loss = 0.0
+            total_sum = 0
+            sum = 0
+            net.cuda().train()
+
+            num_its = len(train_data)
+            for it, (images, labels, indices) in enumerate(train_data, 0):
+
+                logits = net(Variable(images.cuda()))
+                probs = F.sigmoid(logits)
+                loss = multi_criterion(logits, labels.cuda())
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                # additional metrics
+                sum_smooth_loss += loss.data[0]
+                total_epoch_loss += loss.data[0]
+                sum += 1
+                total_sum += 1
+
+                # print statistics
+                if it % print_every_iter == print_every_iter-1:
+                    smooth_loss = sum_smooth_loss/sum
+                    sum_smooth_loss = 0.0
+                    sum = 0
+
+                    train_acc = multi_f_measure(probs.data, labels.cuda())
+                    train_loss = loss.data[0]
+                    print('\r{}   {}    {}   |  {}  | {}  {} | ... '.
+                          format(epoch + it/num_its, it + 1, rate, smooth_loss, train_loss, train_acc),
+                          end='', flush=True)
+
+            total_epoch_loss = total_epoch_loss / total_sum
+            if epoch % epoch_test == epoch_test-1 or epoch == num_epoches-1:
+                net.cuda().eval()
+                test_loss, test_acc = evaluate(net, val_data)
+                print('\r', end='', flush=True)
+                print('{}   {}    {}   |  {}  | {}  {} | {}  {} | {}'.
+                      format(epoch + 1, it + 1, rate, smooth_loss, train_loss, train_acc, test_loss, test_acc,
+                             total_epoch_loss))
+
+                # save if the current loss is better
+                if test_loss < best_test_loss:
+                    print('save {} {}'.format(test_loss, best_test_loss))
+                    torch.save(net.state_dict(), '../models/full_data_{}.pth'.format(name))
+                    best_test_loss = test_loss
+
+            logger.add_record('train_loss', total_epoch_loss)
+            logger.add_record('evaluation_loss', test_loss)
+            logger.add_record('f2_score', test_acc)
+
+            logger.save()
+            logger.save_plot()
+            logger.save_time(start_time=t, end_time=time.time())
+
+
+if __name__ == '__main__':
+    train_baselines()
+
