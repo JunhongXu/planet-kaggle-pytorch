c7d99d0d5edc3320b49a8e7870f7dab4037bd465
Junhong Xu <jxu7@bsu.edu>
2017-07-13 08:03:33 +0800

    resnet50-resnet152+densenets gives ranking 29, train a blender now

--- planet_models/blender.py
+++ planet_models/blender.py
@@ -32,6 +32,7 @@ class Blender(nn.Module):
             l = F.relu(m(x))       # do we need this?
             logits.append(l)
         logits = torch.cat(logits, 1)
+        logits = logits.view(-1, len(models_names) * 17)
         logits = self.weighing(logits)
         return logits
 
